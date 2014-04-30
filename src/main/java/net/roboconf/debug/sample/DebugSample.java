4afb9926e4210ea43dc9ba2c96076b915ee8526b
vzurczak <vincent.zurczak@linagora.com>
2014-04-30 15:30:48 +0200

    Update the sample to show how we can replace things

--- src/main/java/net/roboconf/debug/sample/DebugSample.java
+++ src/main/java/net/roboconf/debug/sample/DebugSample.java
@@ -17,12 +17,16 @@
 package net.roboconf.debug.sample;
 
 import java.io.File;
-import java.io.IOException;
 
+import net.roboconf.core.actions.ApplicationAction;
+import net.roboconf.core.model.helpers.InstanceHelpers;
+import net.roboconf.core.model.runtime.Instance;
+import net.roboconf.dm.environment.iaas.IaasResolver;
+import net.roboconf.dm.management.ManagedApplication;
 import net.roboconf.dm.management.Manager;
-import net.roboconf.dm.management.exceptions.AlreadyExistingException;
-import net.roboconf.dm.management.exceptions.InvalidApplicationException;
 import net.roboconf.dm.rest.client.test.RestTestUtils;
+import net.roboconf.iaas.api.IaasException;
+import net.roboconf.iaas.api.IaasInterface;
 
 import org.junit.Test;
 
@@ -62,16 +66,32 @@ public class DebugSample extends JerseyTest {
 		// Change the directory location for your own project
 		try {
 			Manager.INSTANCE.tryToChangeMessageServerIp( "localhost" );
-			Manager.INSTANCE.loadNewApplication( new File( "D:/Workspaces/WkRoboconfGit/roboconf/roboconf-core/src/test/resources/applications/valid/karaf-joram-jndi" ));
-			// Manager.INSTANCE.loadNewApplication( new File( "/home/vincent/git/roboconf-deployment/linagora-rse" ));
+			ManagedApplication ma = Manager.INSTANCE.loadNewApplication( new File( "D:/lamp-legacy-1" ));
 
-		} catch( AlreadyExistingException e ) {
-			e.printStackTrace();
+			// Use in-memory agents
+			Manager.INSTANCE.setIaasResolver( new IaasResolver() {
+				@Override
+				public IaasInterface findIaasInterface( ManagedApplication ma, Instance instance ) throws IaasException {
+					return super.findIaasHandler( IaasResolver.IAAS_IN_MEMORY );
+				}
+			});
 
-		} catch( InvalidApplicationException e ) {
-			e.printStackTrace();
+			// All the sub-instances will use the logger plug-in
+			for( Instance instance : InstanceHelpers.getAllInstances( ma.getApplication())) {
+				if( instance.getParent() != null )
+					instance.getComponent().setInstallerName( "logger" );
+			}
 
-		} catch( IOException e ) {
+			// Deploy everything automatically
+			for( Instance instance : InstanceHelpers.getAllInstances( ma.getApplication())) {
+				Manager.INSTANCE.perform(
+						ma.getApplication().getName(),
+						ApplicationAction.deploy.toString(),
+						InstanceHelpers.computeInstancePath( instance ),
+						false );
+			}
+
+		} catch( Exception e ) {
 			e.printStackTrace();
 		}
 	}
